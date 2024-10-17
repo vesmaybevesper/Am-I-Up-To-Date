65e0da26a6ee2e1c67ff4d72d8db86da61bf294c
VesMaybeVesper <vessmaybevesper@gmail.com>
2024-10-17 14:44:57 -0400

    version fetching and comparison is still broken otherwise this is bootable

--- src/client/java/vesper/aiutd/AIUTDAmIUpToDateClient.java
+++ src/client/java/vesper/aiutd/AIUTDAmIUpToDateClient.java
@@ -1,25 +1,31 @@
 package vesper.aiutd;
 
-
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.Mouse.*;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.text.Text;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.gui.widget.ClickableWidget;
-import org.jetbrains.annotations.Nullable;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class AIUTDAmIUpToDateClient implements ClientModInitializer {
 	// @Override
-	String modpackVersion = "1.1.4";
+	String modpackVersion = "Placeholder";
+	String currentVersion = "1.1.3";
 	Boolean needUpdate = Boolean.TRUE;
-
+	public static final Logger PRINT_TO_LOGS = LoggerFactory.getLogger("modpackVersion");
 
 	public static class UpdateWidget extends ClickableWidget {
 		public UpdateWidget(int x, int y, int width, int hieght) {
@@ -57,9 +63,47 @@ public class AIUTDAmIUpToDateClient implements ClientModInitializer {
 		}
 
 	}
-	public static final Logger LOGGER = LoggerFactory.getLogger("loaded");
+public class VersionChecker {
+		// grab version from Modrinth API
+		public static String getLatestVersion(){
+			String apiUrl = "https://api.modrinth.com/v2/project/ihN5bmrg/version";
+			StringBuilder result = new StringBuilder();
+			try {
+				URL url = new URL("https://api.modrinth.com/v2/project/ihN5bmrg/version");
 
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+				BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				String line;
+				while ((line = rd.readLine()) != null) {
+					result.append(line);
+				}
+				rd.close();
+
+				JsonArray jsonArray = JsonParser.parseString(result.toString()).getAsJsonArray();
+				if (!jsonArray.isEmpty()) {
+					JsonElement getVersionElement = jsonArray.get(0);
+					String latestVersion = getVersionElement.getAsJsonObject().get("version_number").getAsString();
+					return latestVersion;
+                }
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+}
 	public void onInitializeClient() {
+		modpackVersion = new VersionChecker().toString();
+		if (Objects.equals(modpackVersion, currentVersion)){
+			needUpdate = Boolean.FALSE;
+		}
+		else {
+			needUpdate = Boolean.TRUE;
+			PRINT_TO_LOGS.info(modpackVersion);
+		}
+
 		//pop-up should only display if there is an update
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {if (needUpdate == Boolean.TRUE && client.currentScreen == null) {
 			CustomScreen windowPopup = new CustomScreen(Text.literal("test"));
