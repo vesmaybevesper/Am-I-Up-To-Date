be7eafb6b6ce1294de6f818a6b304447a4ce79a9
VesMaybeVesper <vessmaybevesper@gmail.com>
2024-10-17 18:30:49 -0400

    Renamed files to be more descriptive

--- src/client/java/vesper/aiutd/mixin/client/AmIUpToDateMixin.java
+++ src/client/java/vesper/aiutd/mixin/client/AmIUpToDateMixin.java
@@ -1,15 +1,13 @@
 package vesper.aiutd.mixin.client;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
-import vesper.aiutd.AIUTDAmIUpToDateClient.UpdateWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class ExampleClientMixin {
+public class AmIUpToDateMixin {
 	@Inject(at = @At("HEAD"), method = "run")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftClient.run()V
