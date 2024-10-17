51b2874ed35afcf8f6e6b92132b7bc437341e207
VesMaybeVesper <vessmaybevesper@gmail.com>
2024-10-17 12:44:25 -0400

    First version with working rendering, for fallback purposes.

--- src/client/java/vesper/aiutd/AIUTDAmIUpToDateClient.java
+++ src/client/java/vesper/aiutd/AIUTDAmIUpToDateClient.java
@@ -1,12 +1,18 @@
 package vesper.aiutd;
 
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Mouse.*;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.gui.widget.ClickableWidget;
+import org.jetbrains.annotations.Nullable;
 
 
 public class AIUTDAmIUpToDateClient implements ClientModInitializer {
@@ -14,32 +20,51 @@ public class AIUTDAmIUpToDateClient implements ClientModInitializer {
 	String modpackVersion = "1.1.4";
 	Boolean needUpdate = Boolean.TRUE;
 
-	public static class updateWidget extends ClickableWidget {
-		public updateWidget(int x, int y, int width, int hieght){
-			super(x,y,width,hieght, Text.empty());
+
+	public static class UpdateWidget extends ClickableWidget {
+		public UpdateWidget(int x, int y, int width, int hieght) {
+			super(x, y, width, hieght, Text.empty());
 		}
 
 		@Override
-		protected void renderWidget(DrawContext context,int mouseX,int mouseY,float delta) {
+		protected void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
 			int startColor = 0xFF00FF00;
 			int endColor = 0xFF0000FF;
 
-			context.fillGradient(getX(),getY(), +this.width,getY() + this.height,startColor,endColor);
+			context.fillGradient(getX(), getY(), +this.width, getY() + this.height, startColor, endColor);
 		}
 
 		@Override
 		protected void appendClickableNarrations(NarrationMessageBuilder builder) {
 
 		}
-	};
+	}
 
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	if(needUpdate == Boolean.TRUE) {
-		updateWidget isUpdate = new updateWidget(40, 80, 120, 20);
-		//this.addDrawableChild(isUpdate);
-		System.out.println("Loaded into if");
-    }
+	// some bullshit hacky way to get access to addDrawableChild cause it hates me and is transphobic (or im dumb(this is more likely)).
+	class CustomScreen extends Screen {
+		CustomScreen(Text title) {
+			super(title);
+		}
+
+		@Override
+		protected void init() {
+			super.init();
+
+
+
+			UpdateWidget customWidget = new UpdateWidget(40, 80, 120, 20);
+			this.addDrawableChild(customWidget);
 		}
 
 	}
+	public static final Logger LOGGER = LoggerFactory.getLogger("loaded");
+
+	public void onInitializeClient() {
+		//pop-up should only display if there is an update
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {if (needUpdate == Boolean.TRUE && client.currentScreen == null) {
+			CustomScreen windowPopup = new CustomScreen(Text.literal("test"));
+			MinecraftClient.getInstance().setScreen(windowPopup);
+		}
+		});
+	}
+}
--- src/client/java/vesper/aiutd/mixin/client/ExampleClientMixin.java
+++ src/client/java/vesper/aiutd/mixin/client/ExampleClientMixin.java
@@ -1,6 +1,8 @@
 package vesper.aiutd.mixin.client;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import vesper.aiutd.AIUTDAmIUpToDateClient.UpdateWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
