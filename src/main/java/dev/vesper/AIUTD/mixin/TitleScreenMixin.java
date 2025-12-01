7b12586f86f1e7d6fe7635f96755c645d5399850
VesMaybeVesper <vesmaybevesper@gmail.com>
2025-11-30 19:29:53 -0500

    Update TitleScreenMixin.java

--- src/main/java/dev/vesper/AIUTD/mixin/TitleScreenMixin.java
+++ src/main/java/dev/vesper/AIUTD/mixin/TitleScreenMixin.java
@@ -10,7 +10,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
