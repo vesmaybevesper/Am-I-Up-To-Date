7c5757547e3384204ec76bab025314a0db7491af
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-01-15 22:13:19 -0500

    Auto append ?include_changelog=false if not present

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
-- Fix a mixin crash on NeoForge
+- Auto append ?include_changelog=false to Version API links if not present
+  - Temporary just to make sure everyone gets the fastest parsing
 
 
   TO-DO:
--- src/main/java/dev/vesper/AIUTD/AIUTD.java
+++ src/main/java/dev/vesper/AIUTD/AIUTD.java
@@ -6,6 +6,7 @@ import net.fabricmc.loader.api.FabricLoader;
 //? neoforge {
 /*import net.neoforged.fml.ModList;
 *///?}
+import dev.vesper.AIUTD.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +17,13 @@ public class AIUTD {
 
     public static void init() {
         LOG.info("Initializing {} on {}", MOD_ID, Platform.INSTANCE.loader());
+        // Make sure all config urls get updated, this will go away after a while, it's just to make sure as many people as possible get it
+        Config.HANDLER.load();
+        if (!Config.versionAPI.contains("?include_changelog=false")){
+            LOG.warn("?include_changelog=false not detected in version API string, adding");
+            Config.versionAPI = Config.versionAPI + "?include_changelog=false";
+            Config.HANDLER.save();
+        }
     }
 
     public static boolean isModLoaded(String modId){
