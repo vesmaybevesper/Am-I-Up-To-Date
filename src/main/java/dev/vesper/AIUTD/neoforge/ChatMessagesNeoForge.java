862637f0588e573235b07284165298536bc5c917
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-05-11 15:41:52 -0400

    clean up neo chat message

--- src/main/java/dev/vesper/AIUTD/neoforge/ChatMessagesNeoForge.java
+++ src/main/java/dev/vesper/AIUTD/neoforge/ChatMessagesNeoForge.java
@@ -1,9 +1,10 @@
 package dev.vesper.AIUTD.neoforge;
 
 //? neoforge {
-/*import dev.vesper.AIUTD.config.Config;
+/*import dev.vesper.AIUTD.AIUTD;
+import dev.vesper.AIUTD.common.Util;
+import dev.vesper.AIUTD.config.Config;
 import dev.vesper.AIUTD.config.EndUserConfig;
-import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
@@ -21,89 +22,48 @@ import static dev.vesper.AIUTD.config.Config.*;
 import static dev.vesper.AIUTD.config.EndUserConfig.shouldIgnore;
 
 public class ChatMessagesNeoForge {
-    public void chatMessageInit(){
-
-        NeoForge.EVENT_BUS.register(this);
-    }
+    public void chatMessageInit(){NeoForge.EVENT_BUS.register(this);}
 
     public Component clickableLink(String message, String URL) {
         ClickEvent clickEvent = new ClickEvent.OpenUrl(URI.create(URL));
-        return Component.literal(message).setStyle(Style.EMPTY.withClickEvent(clickEvent).withUnderlined(true).withColor(TextColor.fromLegacyFormat(ChatFormatting.RED)));
+        return Component.literal(message).setStyle(Style.EMPTY.withClickEvent(clickEvent).withUnderlined(true).withColor(Util.changelogColor));
     }
 
+    // I cant remember why this didnt work and i would like to use it
     public Component ignoreMsg() {
         ClickEvent clickEvent = new ClickEvent.RunCommand("/shouldIgnore");
-        return Component.translatable("aiutd.msg.ignoreClickable").setStyle(Style.EMPTY.withClickEvent(clickEvent).withUnderlined(true).withColor(TextColor.fromLegacyFormat(ChatFormatting.GRAY)));
+        return Component.translatable("aiutd.msg.ignoreClickable").setStyle(Style.EMPTY.withClickEvent(clickEvent).withUnderlined(true)).withStyle(Util.ignoreMsgColor);
     }
     public Component toIgnore(){
         return Component.translatable("aiutd.runToIgnore")
                 .setStyle(Style.EMPTY
-                        .withColor(TextColor.fromLegacyFormat(ChatFormatting.GRAY))
-                        .withItalic(true));
+                        .withColor(Util.ignoreMsgColor)
+                        .withItalic(true)).withStyle(Util.ignoreMsgColor);
     }
 
     @SubscribeEvent
     private void chatMessageDisplay(ClientPlayerNetworkEvent.LoggingIn event) {
         EndUserConfig.USERCONFIG.load();
 
-        if (Config.chatAlert && needUpdate) {
+        if (Config.chatAlert && needUpdate && !AIUTD.hasNotified && !shouldIgnore) {
+            Minecraft client = Minecraft.getInstance();
+            assert client.player != null;
 
-            if (useCustomMessage && !Objects.equals(Config.customMessage, "This is a custom message!") && !shouldIgnore) {
-
-                Minecraft minecraft = Minecraft.getInstance();
-                if (minecraft.player != null) {
-                    minecraft.execute(() ->
-                            minecraft.player.sendSystemMessage(Component.literal(Config.customMessage)));
-                }
-
-                if (Config.linkChangelog) {
-                    if (minecraft.player != null) {
-                        minecraft.execute(() ->
-                                minecraft.player.sendSystemMessage(clickableLink("Read the changelog!", changelogLink)));
-
-                    }
-                }
-                assert minecraft.player != null;
-                minecraft.player.sendSystemMessage(toIgnore());
-
+            if (useCustomMessage && !Objects.equals(Config.customMessage, "This is a custom message!")) {
+                client.player.sendSystemMessage(Component.literal(Config.customMessage).withStyle(Util.updateMsgColor));
+            }
+            else if (useModpackName && !Objects.equals(modpackName, "Default") && !useCustomMessage) {
+                client.player.sendSystemMessage(Component.literal(Component.translatable("aiutd.modPackNameMsg").withColor(Objects.requireNonNull(TextColor.fromLegacyFormat(Util.updateMsgColor)).getValue()).getString() + modpackName + "!")));
+            }
+            else {
+                client.player.sendSystemMessage(Component.translatable("aiutd.defaultMsg").withStyle(Util.updateMsgColor)));
             }
 
-            else if (useModpackName && !Objects.equals(modpackName, "Default") && !useCustomMessage && !shouldIgnore) {
-                Minecraft minecraft = Minecraft.getInstance();
-                if (minecraft.player != null) {
-                    minecraft.execute(() ->
-                            minecraft.player.sendSystemMessage(Component.literal(Component.translatable("aiutd.modPackNameMsg").getString() + modpackName + "!")));
-                }
-
-                if (Config.linkChangelog) {
-                    if (minecraft.player != null) {
-                        minecraft.execute(() ->
-                                minecraft.player.sendSystemMessage(clickableLink("Read the changelog!", changelogLink)));
-
-                    }
-                }
-                assert minecraft.player != null;
-                minecraft.player.sendSystemMessage(toIgnore());
+            if (Config.linkChangelog) {
+                client.player.sendSystemMessage(clickableLink("Read the changelog!", changelogLink));
             }
-
-            else if (!shouldIgnore) {
-                Minecraft minecraft = Minecraft.getInstance();
-                if (minecraft.player != null) {
-                    minecraft.execute(() ->
-                            minecraft.player.sendSystemMessage(Component.translatable("aiutd.defaultMsg")));
-                }
-
-                if (Config.linkChangelog) {
-                    if (minecraft.player != null) {
-                        minecraft.execute(() ->
-                                minecraft.player.sendSystemMessage(clickableLink("Read the changelog!", changelogLink)));
-
-                    }
-                }
-                assert minecraft.player != null;
-                minecraft.player.sendSystemMessage(toIgnore());
-
-            }
+            
+            client.player.sendSystemMessage(toIgnore());
         }
     }
 }
