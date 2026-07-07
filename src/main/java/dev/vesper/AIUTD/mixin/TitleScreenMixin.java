cb808d833afdc653aac28a020129673918aa435c
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-07-07 19:49:02 -0400

    rename update notice variables

--- src/main/java/dev/vesper/AIUTD/mixin/TitleScreenMixin.java
+++ src/main/java/dev/vesper/AIUTD/mixin/TitleScreenMixin.java
@@ -1,7 +1,6 @@
 package dev.vesper.AIUTD.mixin;
 
 import dev.vesper.AIUTD.AIUTD;
-import dev.vesper.AIUTD.config.Config;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
@@ -27,43 +26,48 @@ public abstract class TitleScreenMixin extends Screen {
     private int buttonY;
 
     @Inject(method = "createNormalMenuOptions", at = @At("RETURN"))
-    private void addUpdateNotice(int i, int j, CallbackInfoReturnable<Integer> cir) {
+    private void addUpdateNotice(int topPos, int spacing, CallbackInfoReturnable<Integer> cir) {
         if (!AIUTD.isModLoaded("fancymenu")) {
             if(AIUTD.isModLoaded("notebook")){
-                 buttonY = i - 24;
+                 buttonY = topPos - 24;
              } else {
-                 buttonY = i;
+                 buttonY = topPos;
              }
             if (needUpdate && menuAlert){
                 this.addRenderableWidget(
                         Button.builder(Component.translatable("aiutd.menuNotice"), button -> {
-                            try {
-
-                                URI url = new URI(AIUTD.changelogLink);
-                                // Check if the Desktop class is supported and if the browser can be opened
-                                if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-                                    Desktop.getDesktop().browse(url);
-                                } else {
-                                    // alternative link opening logic
-                                    String os = System.getProperty("os.name").toLowerCase();
-                                    try {
-                                        if (os.contains("win")) {
-                                            // link: "https://modrinth.com/modpack/" + {} + "/changelog", Config.modrinthSlug"
-                                            Runtime.getRuntime().exec(new String[]{"rundll32", "url.dll,FileProtocolHandler", AIUTD.changelogLink});
-                                        } else if (os.contains("mac")) {
-                                            Runtime.getRuntime().exec(new String[]{"open", AIUTD.changelogLink});
-                                        } else if (os.contains("nix") || os.contains("nux")) {
-                                            Runtime.getRuntime().exec(new String[]{"xdg-open", AIUTD.changelogLink});
-                                        } else {
-                                            AIUTD.LOG.error("Unsupported OS for opening a browser.");
+                            /*if (AIUTD.isModLoaded("mcef")) {
+                                //? <26.2{
+                                *//*Minecraft.getInstance().setScreen(new MCEFWindow(Component.literal("Modpack Changelog")));
+                                *//*//?} >=26.2{
+                                Minecraft.getInstance().gui.setScreen(new MCEFWindow(Component.literal("Modpack Changelog")));
+                                //?}
+                            } else {*/
+                                try {
+                                    URI url = new URI(AIUTD.changelogLink);
+                                    // Check if the Desktop class is supported and if the browser can be opened
+                                    if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                                        Desktop.getDesktop().browse(url);
+                                    } else {
+                                        // alternative link opening logic
+                                        String os = System.getProperty("os.name").toLowerCase();
+                                        try {
+                                            if (os.contains("win")) {
+                                                Runtime.getRuntime().exec(new String[]{"rundll32", "url.dll,FileProtocolHandler", AIUTD.changelogLink});
+                                            } else if (os.contains("mac")) {
+                                                Runtime.getRuntime().exec(new String[]{"open", AIUTD.changelogLink});
+                                            } else if (os.contains("nix") || os.contains("nux")) {
+                                                Runtime.getRuntime().exec(new String[]{"xdg-open", AIUTD.changelogLink});
+                                            } else {
+                                                AIUTD.LOG.error("Unsupported OS for opening a browser.");
+                                            }
+                                        }    catch (IOException e) {
+                                            AIUTD.LOG.info(String.valueOf(e));
                                         }
-                                    }    catch (IOException e) {
-                                        AIUTD.LOG.info(String.valueOf(e));
                                     }
+                                } catch (Exception e) {
+                                    AIUTD.LOG.error(String.valueOf(e));
                                 }
-                            } catch (Exception e) {
-                                AIUTD.LOG.info(String.valueOf(e));
-                            }
                         })
                                 .bounds(this.width / 2 - 100 + 205, buttonY, 90, 20)
                                 .build());
