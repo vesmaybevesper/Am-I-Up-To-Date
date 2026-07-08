abe5d8c7852f02cc9d80ddc53c09ce8f577e9e91
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-07-08 13:40:40 -0400

    remove a comment that was just a note for myself

--- src/main/java/dev/vesper/AIUTD/mixin/TitleScreenMixin.java
+++ src/main/java/dev/vesper/AIUTD/mixin/TitleScreenMixin.java
@@ -54,7 +54,6 @@ public abstract class TitleScreenMixin extends Screen {
                                     String os = System.getProperty("os.name").toLowerCase();
                                     try {
                                         if (os.contains("win")) {
-                                            // link: "https://modrinth.com/modpack/" + {} + "/changelog", Config.modrinthSlug"
                                             Runtime.getRuntime().exec(new String[]{"rundll32", "url.dll,FileProtocolHandler", AIUTD.changelogLink});
                                         } else if (os.contains("mac")) {
                                             Runtime.getRuntime().exec(new String[]{"open", AIUTD.changelogLink});
