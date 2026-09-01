31d8555c80746bc1a7ec1a343004c79b7ca574a0
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-09-01 11:18:42 -0400

    fixed a missing import

--- src/main/java/dev/vesper/aiutd/mixin/TitleScreenMixin.java
+++ src/main/java/dev/vesper/aiutd/mixin/TitleScreenMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static dev.vesper.aiutd.common.UpdateChecker.needUpdate;
